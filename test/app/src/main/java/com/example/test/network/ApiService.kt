f92c2c20a37983051e02571838059cefd5dac76d
(Jaemin) <(kjm9049123@gmail.com)>
2025-02-04 20:15:07 +0900

    2/4 ApiServcei 수정

--- test/app/src/main/java/com/example/test/network/ApiService.kt
+++ test/app/src/main/java/com/example/test/network/ApiService.kt
@@ -2,21 +2,26 @@ package com.example.test.network
 
 
 import com.example.test.model.ApiResponse
+import com.example.test.model.LoginInfoResponse
 import com.example.test.model.LoginRequest
 import com.example.test.model.LoginResponse
 import com.example.test.model.SignUpRequest
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface ApiService {
 
     // 로그인 요청
-    @POST("api/auth/login")
+    @POST("login")
     fun login(@Body loginRequest: LoginRequest): Call<LoginResponse>
 
     // 회원가입 요청
-    @POST("api/auth/register")
+    @POST("join")
     fun signUp(@Body signUpRequest: SignUpRequest): Call<ApiResponse>
-    abstract fun getUserInfo(s: String): Any
+
+    @GET("user/info")
+    fun getUserInfo(@Header("Authorization") token: String): Call<LoginInfoResponse>
 }
