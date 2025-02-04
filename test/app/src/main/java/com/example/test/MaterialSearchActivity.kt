ce34ed59c6e65d31531ec6995f90b39ca1b92fe3
duswn <duswndi1106@gmail.com>
2025-02-04 20:43:14 +0900

    qwer

--- test/app/src/main/java/com/example/test/MaterialSearchActivity.kt
+++ test/app/src/main/java/com/example/test/MaterialSearchActivity.kt
@@ -3,6 +3,7 @@ package com.example.test
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
@@ -13,7 +14,7 @@ class MaterialSearchActivity : AppCompatActivity() {
         setContentView(R.layout.activity_material_search) // MaterialDetailActivity의 레이아웃 파일 연결
 
         // seach을 클릭했을 때 MaterialSearchDetailActivity 이동
-        val seach: LinearLayout = findViewById(R.id.seach)
+        val seach: ImageView = findViewById(R.id.seach)
         seach.setOnClickListener {
             val intent = Intent(this, MaterialSearchDetailActivity::class.java)
             startActivity(intent)
