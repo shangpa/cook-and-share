70954c26a08ce9148e40de8090a8ca296b7edb5e
jihyunArchive <ojhyeon930@naver.com>
2025-02-17 01:06:53 +0900

    2/17 작성한 레시피, 레시피 리뷰 내역, 냉장고 재료 내역, 저장한 게시글, 작성한 게시글 프론트 추가 및 기타 수정

--- test/app/src/main/java/com/example/test/MypageActivity.kt
+++ test/app/src/main/java/com/example/test/MypageActivity.kt
@@ -8,7 +8,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class MypageActivity : AppCompatActivity()  {
+class MypageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mypage)
