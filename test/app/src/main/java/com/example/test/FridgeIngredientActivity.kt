e770f7090a2d7a73666fa7491e8fe98d7a220ac1
jihyunArchive <ojhyeon930@naver.com>
2025-03-05 01:31:58 +0900

    3/15 냉장고 및 세부 페이지 + 로그인 수정

--- test/app/src/main/java/com/example/test/FridgeIngredientActivity.kt
+++ test/app/src/main/java/com/example/test/FridgeIngredientActivity.kt
@@ -3,15 +3,14 @@ package com.example.test
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
-class FridgeMaterialActivity  : AppCompatActivity() {
+class FridgeIngredientActivity  : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fridge_material)
+        setContentView(R.layout.activity_fridge_ingredient)
 
         // fridgeAddButton 클릭했을 때 MaterialSalesActivity 이동
         val fridgeAddButton: LinearLayout = findViewById(R.id.fridgeAddButton)
@@ -20,9 +19,9 @@ class FridgeMaterialActivity  : AppCompatActivity() {
             startActivity(intent)
         }
 
-        // fridgeMaterialBackButton 클릭했을 때 MaterialSalesActivity 이동
-        val fridgeMaterialBackButton: ImageView = findViewById(R.id.fridgeMaterialBackButton)
-        fridgeMaterialBackButton.setOnClickListener {
+        // backBtn 클릭했을 때 MaterialSalesActivity 이동
+        val backBtn: ImageView = findViewById(R.id.backBtn)
+        backBtn.setOnClickListener {
             val intent = Intent(this, FridgeActivity::class.java)
             startActivity(intent)
         }
