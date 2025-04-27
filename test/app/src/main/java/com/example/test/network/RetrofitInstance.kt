bf7b1a28febd63f05bc284e245ab14c2cfa718b3
yeojin <jeyojin565@gmail.com>
2025-04-27 14:56:05 +0900

    메인 수정, 음성 수정

--- test/app/src/main/java/com/example/test/network/RetrofitInstance.kt
+++ test/app/src/main/java/com/example/test/network/RetrofitInstance.kt
@@ -7,7 +7,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    const val BASE_URL = "http://192.168.69.42:8080"
+    const val BASE_URL = "http://192.168.0.19:8080"
 
     private lateinit var retrofit: Retrofit
     lateinit var apiService: ApiService
