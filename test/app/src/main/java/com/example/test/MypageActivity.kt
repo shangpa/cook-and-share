1cff265f6a276d43712129c2095db42fb18677b2
jihyunArchive <ojhyeon930@naver.com>
2025-05-19 01:09:38 +0900

    마이페이지 선호요리 삭제

--- test/app/src/main/java/com/example/test/MypageActivity.kt
+++ test/app/src/main/java/com/example/test/MypageActivity.kt
@@ -28,12 +28,6 @@ class MypageActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        // likeFoodIcon 클릭했을 때 MainActivity 이동
-        val likeFoodIcon: ImageView = findViewById(R.id.likeFoodIcon)
-        likeFoodIcon.setOnClickListener {
-            val intent = Intent(this, LikeFoodActivity::class.java)
-            startActivity(intent)
-        }
 
         // logoutText 클릭했을 때 LoginActivity 이동
         val logoutText: TextView = findViewById(R.id.logoutText)
