1d5dd8ae48249c440b67e147885fc9d8e3da47fd
kjm9049123@gmail.com <kjm9049123@gmail.com>
2025-06-16 17:38:57 +0900

    메니페스트 수정

--- test/app/src/main/java/com/example/test/network/RetrofitInstance.kt
+++ test/app/src/main/java/com/example/test/network/RetrofitInstance.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit
 
 object RetrofitInstance {
 
-    const val BASE_URL = "http://172.30.1.84:8080"
+    const val BASE_URL = "http://192.168.0.91:8080"
 
     private lateinit var retrofit: Retrofit
     lateinit var apiService: ApiService
